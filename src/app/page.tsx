e2dfa16497f2424e19eb544a740fcbc7cc4ba1fe
khaan <nguyenkhaan2006@gmail.com>
2026-02-02 20:31:55 +0700

    chore: initial project folder structure

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/navbar"
+const Home = () => 
+{   
+    return(
+        <>
+            <Navbar /> 
+            <div>
+                Xin chao cac ban
+            </div>
+        
+        </>
+    )
+}
+export default Home
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => 
+{
+    return (
+        <div className="w-screen fixed top-0 bg-blue-500 h-20 right-0 left-0 shadow-xl px-8">
+            Day la navbar
+        </div>
+    )
+}
+export default Navbar
